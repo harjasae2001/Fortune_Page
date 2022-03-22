34c804dd0136f74845f6e270edffe39fb6d599f3
Harjas <harjassingh_co20b4_07@dtu.ac.in>
2022-03-22 17:34:58 +0530

    countdown Timer Added

---
+++ script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var dest = new Date("April 30, 2022 10:00:00").getTime();
+
+var x = setInterval(function () {
+  var now = new Date().getTime();
+
+  var diff = dest - now;
+
+  var days = Math.floor(diff / (86400 * 1000));
+  //   console.log(days);
+
+  var hours = Math.floor((diff % (1000 * 86400)) / (1000 * 60 * 60));
+  //   console.log(hours);
+
+  var minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  //   console.log(minutes);
+  var seconds = Math.floor((diff % (1000 * 60)) / 1000);
+  console.log(seconds);
+
+  document.querySelector(".demo").innerHTML =
+    days + " d : " + hours + " h : " + minutes + " m : " + seconds + " s";
+}, 1000);
